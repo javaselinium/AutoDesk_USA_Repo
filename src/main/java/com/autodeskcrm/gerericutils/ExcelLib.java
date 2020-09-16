2a4875125f8d7a8d011e98ec6b16c3771af02e0c
Deepak <Deepak@Ty-Deepak-Tr.lan>
2020-09-16 13:46:22 +0530

    modified

--- src/main/java/com/autodeskcrm/gerericutils/ExcelLib.java
+++ src/main/java/com/autodeskcrm/gerericutils/ExcelLib.java
@@ -14,7 +14,7 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
  *
  */
 public class ExcelLib {
-	String filePAth = "./testData/testScriptData.xlsx";
+	
    /**
     * used to read the data from excel workook based on
     * @param shettName
@@ -24,7 +24,7 @@ public class ExcelLib {
     * @throws Throwable
     */
 	public String getExcelData(String filePath ,String shettName , int rowNum , int colNum) throws Throwable {
-		FileInputStream fis = new FileInputStream(filePAth);
+		FileInputStream fis = new FileInputStream(filePath);
 		Workbook wb = WorkbookFactory.create(fis);
 		Sheet sh = wb.getSheet(shettName);
 		Row row = sh.getRow(rowNum);
@@ -41,14 +41,14 @@ public class ExcelLib {
 	 * @throws Throwable
 	 */
 	
-	public void setExcelData(String shettName , int rowNum , int colNum ,String data) throws Throwable {
-		FileInputStream fis = new FileInputStream(filePAth);
+	public void setExcelData(String filePath, String shettName , int rowNum , int colNum ,String data) throws Throwable {
+		FileInputStream fis = new FileInputStream(filePath);
 		Workbook wb = WorkbookFactory.create(fis);
 		Sheet sh = wb.getSheet(shettName);
 		Row row = sh.getRow(rowNum);
 		Cell cel = row.createCell(colNum);
 		cel.setCellValue(data);
-		FileOutputStream fos = new FileOutputStream(filePAth);
+		FileOutputStream fos = new FileOutputStream(filePath);
 				wb.write(fos);
 		wb.close();
 	}
@@ -58,8 +58,8 @@ public class ExcelLib {
 	 * @return
 	 * @throws Throwable
 	 */
-	public int getRowCount(String shettName)throws Throwable {
-		FileInputStream fis = new FileInputStream(filePAth);
+	public int getRowCount(String filePath, String shettName)throws Throwable {
+		FileInputStream fis = new FileInputStream(filePath);
 		Workbook wb = WorkbookFactory.create(fis);
 		Sheet sh = wb.getSheet(shettName);
 	
