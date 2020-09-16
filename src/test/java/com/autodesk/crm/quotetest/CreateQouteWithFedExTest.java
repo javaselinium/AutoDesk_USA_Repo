21ba800f0d04fba57874e0f40e0018510f374fa8
Deepak <Deepak@Ty-Deepak-Tr.lan>
2020-09-16 13:11:24 +0530

    push qoute Package

---
+++ src/test/java/com/autodesk/crm/quotetest/CreateQouteWithFedExTest.java
@@ -0,0 +1,12 @@
+package com.autodesk.crm.quotetest;
+
+import org.testng.annotations.Test;
+
+public class CreateQouteWithFedExTest {
+	
+	@Test
+	public void createQutoWithFedExTest() {
+		
+	}
+
+}
